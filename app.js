d0c74f8094faa149de72c93703fa8199284bc5f3
EloK6 <ecassignol@gmail.com>
2019-07-28 09:14:47 +0200

    Main character

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ function draw() {
 			ctx.fillRect(55 + i * 15, 95, 4, 4);
 		}
 
-		//Ghost
-		// ctx.beginPath();
+		//Ghost-form
+		ctx.beginPath();
 		ctx.moveTo(83, 116);
 		ctx.lineTo(83, 102);
 		ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
@@ -55,7 +55,7 @@ function draw() {
 		ctx.lineTo(83, 116);
 		ctx.fill();
 
-		// Marks for drawing the curve
+		// Marks for drawing the bezierCurve
 		// ctx.beginPath();
 		// ctx.moveTo(83, 116);
 		// ctx.lineTo(83, 102);
@@ -73,6 +73,48 @@ function draw() {
 		// ctx.lineTo(87, 111);
 		// ctx.lineTo(83, 116);
 		// ctx.stroke();
+
+		//Ghost-eyes
+		ctx.fillStyle = "white";
+		ctx.beginPath();
+		ctx.moveTo(91, 96);
+		ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+		ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+		ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+		ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+		ctx.moveTo(103, 96);
+		ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+		ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+		ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+		ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+		ctx.fill();
+
+		// Marks for drawing the bezierCurve
+		// ctx.fillStyle = "white";
+		// ctx.beginPath();
+		// ctx.moveTo(91, 96);
+		// ctx.lineTo(88, 96);
+		// ctx.lineTo(87, 99);
+		// ctx.lineTo(87, 101);
+		// ctx.lineTo(87, 103);
+		// ctx.lineTo(88, 106);
+		// ctx.lineTo(91, 106);
+		// ctx.lineTo(94, 106);
+		// ctx.lineTo(95, 103);
+		// ctx.lineTo(95, 101);
+		// ctx.lineTo(95, 99);
+		// ctx.lineTo(94, 96);
+		// ctx.lineTo(91, 96);
+		// ctx.fill();
+
+		ctx.fillStyle = "black";
+		ctx.beginPath();
+		ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+		ctx.fill();
 	}
 }
 
